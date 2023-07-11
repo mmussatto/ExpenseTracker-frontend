02cd77a959c60e1fcea7a84b5f1e61658d0d68e2
mmussatto <murilomussatto@gmail.com>
2023-07-11 17:01:48 -0300

    Add nav component with material side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, FooterComponent],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -21,6 +23,7 @@ import { FooterComponent } from './components/footer/footer.component';
         MatIconModule,
         MatButtonModule,
         MatToolbarModule,
+        MatSidenavModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
