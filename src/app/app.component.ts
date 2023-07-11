3a3199a2a78655216236757e430bef8db73b552c
mmussatto <murilomussatto@gmail.com>
2023-07-11 17:16:27 -0300

    Open sidenav using the header button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'ExpenseTracker-frontend';
+    isNavOpen: boolean = false;
+
+    toggleNav(event: boolean) {
+        this.isNavOpen = !this.isNavOpen;
+        console.log("toggleNav", event);
+    }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, ViewChild } from "@angular/core";
+import { MatDrawer } from "@angular/material/sidenav";
 
 @Component({
     selector: "app-nav",
     templateUrl: "./nav.component.html",
     styleUrls: ["./nav.component.css"],
 })
-export class NavComponent {}
+export class NavComponent implements OnChanges {
+    @ViewChild("drawer")
+    public sidenav?: MatDrawer;
+
+    @Input()
+    isNavOpen?: boolean;
+
+    ngOnChanges(): void {
+        console.log("ngOnChanges", this.isNavOpen);
+        if (this.isNavOpen) {
+            this.sidenav?.open();
+        } else {
+            this.sidenav?.close();
+        }
+    }
+}
