d1c0e5f9275391bb77469946fe88433e63d0eb06
mmussatto <murilomussatto@gmail.com>
2023-07-12 18:24:43 -0300

    Add routerModule and button with a link to categories/new

--- src/app/views/categories/categories.module.ts
+++ src/app/views/categories/categories.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
 //Components
 import { CategoriesComponent } from "./components/categories/categories.component";
@@ -14,17 +15,23 @@ import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatIconModule } from "@angular/material/icon";
+
+const ROUTES: Routes = [{ path: "new", component: NewCategoryComponent }];
 
 @NgModule({
     declarations: [CategoriesComponent, NewCategoryComponent],
     imports: [
         CommonModule,
         HttpClientModule,
+        RouterModule.forChild(ROUTES),
         MatTableModule,
         MatPaginatorModule,
         MatSortModule,
         MatProgressSpinnerModule,
+        MatIconModule,
     ],
     providers: [CategoriesService],
+    exports: [RouterModule],
 })
 export class CategoriesModule {}
