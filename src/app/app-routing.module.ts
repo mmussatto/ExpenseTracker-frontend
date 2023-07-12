8a68bf4672cf9be8966525955087c230f8fba532
mmussatto <murilomussatto@gmail.com>
2023-07-12 09:44:50 -0300

    Add categories path and router-outlet to nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CategoriesComponent } from "./views/categories/components/categories/categories.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "categories",
+        component: CategoriesComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
