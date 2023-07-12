6bb428d2a017582d65b55eccf441e1b96d96ce6d
mmussatto <murilomussatto@gmail.com>
2023-07-12 11:30:55 -0300

    Display the categories info in the mat table

--- src/app/views/categories/components/categories/categories.component.ts
+++ src/app/views/categories/components/categories/categories.component.ts
@@ -1,46 +1,43 @@
 import { LiveAnnouncer } from "@angular/cdk/a11y";
-import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort, Sort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
-
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-    { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-    { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-    { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-    { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-    { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-    { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-    { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-    { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-    { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-];
+import { CategoriesService, Category } from "../../services/categories.service";
 
 @Component({
     selector: "app-categories",
     templateUrl: "./categories.component.html",
     styleUrls: ["./categories.component.css"],
 })
-export class CategoriesComponent implements AfterViewInit {
-    displayedColumns: string[] = ["position", "name", "weight", "symbol"];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class CategoriesComponent implements OnInit, AfterViewInit {
+    displayedColumns: string[] = ["id", "name", "color"];
 
-    constructor(private _liveAnnouncer: LiveAnnouncer) {}
+    categories: Category[] = [];
+    dataSource = new MatTableDataSource(this.categories);
+
+    constructor(
+        private categoriesService: CategoriesService,
+        private _liveAnnouncer: LiveAnnouncer
+    ) {}
+
+    ngOnInit(): void {
+        this.categoriesService.findAllCategories().subscribe((response) => {
+            this.categories = response;
+            console.log(response);
+            this.dataSource = new MatTableDataSource(this.categories);
+        });
+    }
 
     @ViewChild(MatSort) sort?: MatSort | null;
 
     @ViewChild(MatPaginator) paginator?: MatPaginator;
 
     ngAfterViewInit() {
-        this.dataSource.sort = this.sort ?? null;
         this.dataSource.paginator = this.paginator ?? null;
+        this.dataSource.sort = this.sort ?? null;
+        console.log(this.sort);
+        console.log(this.dataSource.sort);
     }
 
     /** Announce the change in sort state for assistive technology. */
