2e5fcc9dacf159f196e0c196de1f053826da067b
mmussatto <murilomussatto@gmail.com>
2023-07-12 19:19:34 -0300

    Add reactive forms to new category component

--- src/app/views/categories/categories.module.ts
+++ src/app/views/categories/categories.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 
 //Components
@@ -16,6 +17,10 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
 
 const ROUTES: Routes = [{ path: "new", component: NewCategoryComponent }];
 
@@ -24,12 +29,17 @@ const ROUTES: Routes = [{ path: "new", component: NewCategoryComponent }];
     imports: [
         CommonModule,
         HttpClientModule,
+        ReactiveFormsModule,
         RouterModule.forChild(ROUTES),
         MatTableModule,
         MatPaginatorModule,
         MatSortModule,
         MatProgressSpinnerModule,
         MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
     ],
     providers: [CategoriesService],
     exports: [RouterModule],
--- src/app/views/categories/components/new-category/new-category.component.ts
+++ src/app/views/categories/components/new-category/new-category.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-new-category",
     templateUrl: "./new-category.component.html",
     styleUrls: ["./new-category.component.css"],
 })
-export class NewCategoryComponent {}
+export class NewCategoryComponent implements OnInit {
+    colors = ["GREY", "PURPLE", "RED", "BLUE"];
+
+    newCategoryForm = this.fb.group({
+        name: [null, Validators.required],
+        color: [null, Validators.required],
+    });
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {}
+
+    submit() {
+        if (!this.newCategoryForm?.valid) {
+            return;
+        }
+        console.log(this.newCategoryForm?.value);
+    }
+}
