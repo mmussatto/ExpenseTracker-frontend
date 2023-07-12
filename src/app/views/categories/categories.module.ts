b8ae9d20de1c3ec2455ea85506d8c2832d4ec556
mmussatto <murilomussatto@gmail.com>
2023-07-12 10:14:14 -0300

    Add mat-table example with sorting and pagination

--- src/app/views/categories/categories.module.ts
+++ src/app/views/categories/categories.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
+//Components
 import { CategoriesComponent } from "./components/categories/categories.component";
+
+//Angular Material
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
 
 @NgModule({
     declarations: [CategoriesComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
 })
 export class CategoriesModule {}
