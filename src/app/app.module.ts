f9ace408b32c81dc45c3e4641d4de29bf09fb7b5
mmussatto <murilomussatto@gmail.com>
2023-07-12 09:44:32 -0300

    Create categories module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+//App components
+import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavComponent } from "./components/nav/nav.component";
+
+//View modules
+import { CategoriesModule } from "./views/categories/categories.module";
 
 //Angular Material
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from "./components/footer/footer.component";
-import { NavComponent } from "./components/nav/nav.component";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 
@@ -21,6 +25,7 @@ import { MatListModule } from "@angular/material/list";
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
+        CategoriesModule,
         MatIconModule,
         MatButtonModule,
         MatToolbarModule,
---
+++ src/app/views/categories/categories.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CategoriesComponent } from "./components/categories/categories.component";
+
+@NgModule({
+    declarations: [CategoriesComponent],
+    imports: [CommonModule],
+})
+export class CategoriesModule {}
