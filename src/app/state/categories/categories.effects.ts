5b579542156996a653b58c7a3127acbe4837261c
mmussatto <murilomussatto@gmail.com>
2023-07-14 12:55:27 -0300

    Create effects to persist the new category in the back end #29

--- src/app/state/categories/categories.effects.ts
+++ src/app/state/categories/categories.effects.ts
@@ -4,7 +4,9 @@ import { Store } from "@ngrx/store";
 import { CategoriesService } from "src/app/views/categories/services/categories.service";
 import { AppState } from "../app.state";
 import {
-    addCategory,
+    createCategory,
+    createCategoryFail,
+    createCategorySuccess,
     deleteCategory,
     deleteCategoryFail,
     deleteCategorySuccess,
@@ -22,7 +24,7 @@ export class CategoriesEffects {
         private categoriesService: CategoriesService
     ) {}
 
-    // Run this code when a loadCategories action is dispatched
+    /* -------- Loading ---------- */
     loadCategories$ = createEffect(() =>
         this.actions$.pipe(
             ofType(loadCategories),
@@ -38,12 +40,40 @@ export class CategoriesEffects {
         )
     );
 
-    deleteCategories$ = createEffect(() =>
+    /* -------- Creating ---------- */
+    createCategory$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(createCategory),
+            switchMap(({ category }) =>
+                // Call the createNewCategory method, convert it to an observable
+                from(this.categoriesService.createNewCategory(category)).pipe(
+                    //If success, show snackbar and redirect to categories list page
+                    // Take the returned value and return a new success action containing the new Category
+                    map((category) => createCategorySuccess({ category: category })),
+                    // Or... if it errors return a new failure action containing the error message
+                    catchError((error) => {
+                        const errorMessage = error.error.message ?? error.error.messages;
+                        return of(
+                            createCategoryFail({
+                                error: errorMessage, //todo, remove after standardization of backend
+                            })
+                        );
+                    })
+                )
+            )
+        )
+    );
+
+    /* -------- Deleting ---------- */
+    deleteCategory$ = createEffect(() =>
         this.actions$.pipe(
             ofType(deleteCategory),
             switchMap(({ id }) =>
+                // Call the deleteCategory method, convert it to an observable
                 from(this.categoriesService.deleteCategory(id)).pipe(
+                    // Return a new success action containing the deleted category's id
                     map(() => deleteCategorySuccess({ id })),
+                    // Or... if it errors return a new failure action containing the error message
                     catchError((error) => {
                         return of(
                             deleteCategoryFail({
