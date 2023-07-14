736529bd82ea7f70668c5fdc92de93415b678854
mmussatto <murilomussatto@gmail.com>
2023-07-14 12:56:22 -0300

    Remove category interface from service (its in the models folder)

--- src/app/views/categories/services/categories.service.ts
+++ src/app/views/categories/services/categories.service.ts
@@ -1,13 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { Category } from "src/app/models/category.model";
 import { environment } from "src/environments/environment";
-
-export interface Category {
-    id?: number;
-    name: string;
-    color: string;
-}
 
 @Injectable({
     providedIn: "root",
