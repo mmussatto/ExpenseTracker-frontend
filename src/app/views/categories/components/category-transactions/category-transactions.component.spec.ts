53fd5705b67c5aa3db0b9ff4935e5fb12fb444c3
mmussatto <murilomussatto@gmail.com>
2023-07-18 15:22:27 -0300

    Create category transactions component Closes #43

---
+++ src/app/views/categories/components/category-transactions/category-transactions.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { CategoryTransactionsComponent } from "./category-transactions.component";
+
+describe("CategoryTransactionsComponent", () => {
+    let component: CategoryTransactionsComponent;
+    let fixture: ComponentFixture<CategoryTransactionsComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [CategoryTransactionsComponent],
+        });
+        fixture = TestBed.createComponent(CategoryTransactionsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/views/categories/components/category-transactions/category-transactions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-category-transactions",
+    templateUrl: "./category-transactions.component.html",
+    styleUrls: ["./category-transactions.component.css"],
+})
+export class CategoryTransactionsComponent {}
