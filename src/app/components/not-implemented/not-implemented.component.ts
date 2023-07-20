8beb552059df3e58c2818c3797b027ee338d0e13
mmussatto <murilomussatto@gmail.com>
2023-07-20 11:18:21 -0300

    Add go back button in the not implemented page Closes #68

--- src/app/components/not-implemented/not-implemented.component.ts
+++ src/app/components/not-implemented/not-implemented.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
+import { Location } from "@angular/common";
 
 @Component({
     selector: "app-not-implemented",
     templateUrl: "./not-implemented.component.html",
     styleUrls: ["./not-implemented.component.css"],
 })
-export class NotImplementedComponent {}
+export class NotImplementedComponent {
+    constructor(private _location: Location) {}
+
+    goBack() {
+        this._location.back();
+    }
+}
