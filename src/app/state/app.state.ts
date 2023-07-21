4eefddfefbd69fea94db365652808eb45787be65
mmussatto <murilomussatto@gmail.com>
2023-07-21 11:43:48 -0300

    Add tag state to app state

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,9 +1,11 @@
 import { CategoriesState } from "./categories/categories.reducer";
 import { PaymentMethodsState } from "./payment-methods/payment-methods.reducer";
+import { TagsState } from "./tags/tags.reducer";
 import { VendorsState } from "./vendors/vendors.reducer";
 
 export interface AppState {
     categories: CategoriesState;
     paymentMethods: PaymentMethodsState;
     vendors: VendorsState;
+    tags: TagsState;
 }
