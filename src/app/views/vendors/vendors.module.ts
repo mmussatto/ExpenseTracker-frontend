7b62f4f29acc78e160a5a9258a57058f1012d610
mmussatto <murilomussatto@gmail.com>
2023-07-21 11:18:39 -0300

    Create vendors module and service

---
+++ src/app/views/vendors/vendors.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { VendorsService } from "./services/vendors.service";
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+    providers: [VendorsService],
+})
+export class VendorsModule {}
