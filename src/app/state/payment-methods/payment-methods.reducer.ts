179ad55ddba349365c85bfd66387bce6b0935b10
mmussatto <murilomussatto@gmail.com>
2023-07-21 10:40:34 -0300

    Create payment method reducer and state

---
+++ src/app/state/payment-methods/payment-methods.reducer.ts
@@ -0,0 +1,102 @@
+import { createReducer, on } from "@ngrx/store";
+
+import { PaymentMethod } from "src/app/models/payment-method.model";
+import {
+    createPaymentMethod,
+    createPaymentMethodFail,
+    createPaymentMethodSuccess,
+    deletePaymentMethod,
+    deletePaymentMethodFail,
+    deletePaymentMethodSuccess,
+    loadPaymentMethods,
+    loadPaymentMethodsFail,
+    loadPaymentMethodsSuccess,
+    updatePaymentMethod,
+    updatePaymentMethodFail,
+    updatePaymentMethodSuccess,
+} from "./payment-methods.actions";
+
+/** PaymentMethods State */
+export interface PaymentMethodsState {
+    paymentMethods: PaymentMethod[];
+    error: string | null;
+    status: "NOT_LOADED" | "LOADING" | "LOADED" | "ERROR" | "PENDING";
+}
+
+export const initialState: PaymentMethodsState = {
+    paymentMethods: [],
+    error: null,
+    status: "NOT_LOADED",
+};
+
+export const paymentMethodsReducer = createReducer(
+    initialState,
+
+    /* -------- Loading ---------- */
+    on(loadPaymentMethods, (state) => ({ ...state, status: "LOADING" as const })),
+
+    on(loadPaymentMethodsSuccess, (state, { paymentMethods }) => ({
+        ...state,
+        paymentMethods: paymentMethods,
+        error: null,
+        status: "LOADED" as const,
+    })),
+
+    on(loadPaymentMethodsFail, (state, { error }) => ({
+        ...state,
+        error: error,
+        status: "ERROR" as const,
+    })),
+
+    /* -------- Creating ---------- */
+    on(createPaymentMethod, (state) => ({ ...state, status: "PENDING" as const })),
+
+    on(createPaymentMethodSuccess, (state, { paymentMethod }) => ({
+        ...state,
+        categories: [...state.paymentMethods, { ...paymentMethod }],
+        error: null,
+        status: "LOADED" as const,
+    })),
+
+    on(createPaymentMethodFail, (state, { error }) => ({
+        ...state,
+        error: error,
+        status: "ERROR" as const,
+    })),
+
+    /* -------- Updating ---------- */
+    on(updatePaymentMethod, (state) => ({ ...state, status: "PENDING" as const })),
+
+    on(updatePaymentMethodSuccess, (state, { paymentMethod }) => ({
+        ...state,
+        categories: state.paymentMethods.map((value) =>
+            value.id === paymentMethod.id
+                ? { ...value, name: paymentMethod.name, color: paymentMethod.type }
+                : value
+        ),
+        error: null,
+        status: "LOADED" as const,
+    })),
+
+    on(updatePaymentMethodFail, (state, { error }) => ({
+        ...state,
+        error: error,
+        status: "ERROR" as const,
+    })),
+
+    /* -------- Deleting ---------- */
+    on(deletePaymentMethod, (state, { id }) => ({ ...state, status: "PENDING" as const })),
+
+    on(deletePaymentMethodSuccess, (state, { id }) => ({
+        ...state,
+        categories: state.paymentMethods.filter((p) => p.id != id),
+        error: null,
+        status: "LOADED" as const,
+    })),
+
+    on(deletePaymentMethodFail, (state, { error }) => ({
+        ...state,
+        error: error,
+        status: "ERROR" as const,
+    }))
+);
