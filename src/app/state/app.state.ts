5b00cfde03295c786951d263cb8a3126a1d2c4fb
mmussatto <murilomussatto@gmail.com>
2023-07-21 13:55:29 -0300

    Add transactions state to app state

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,6 +1,7 @@
 import { CategoriesState } from "./categories/categories.reducer";
 import { PaymentMethodsState } from "./payment-methods/payment-methods.reducer";
 import { TagsState } from "./tags/tags.reducer";
+import { TransactionsState } from "./transactions/transactions.reducer";
 import { VendorsState } from "./vendors/vendors.reducer";
 
 export interface AppState {
@@ -8,4 +9,5 @@ export interface AppState {
     paymentMethods: PaymentMethodsState;
     vendors: VendorsState;
     tags: TagsState;
+    transactions: TransactionsState;
 }
